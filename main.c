3e5fd88b16e18c8d2d66f408f6a1384c93aba232
Srajan Rai <srajaneh@gmail.com>
2022-07-28 11:17:51 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "base.h"
+
+int main(void) {
+    system("cls");
+    printf("Hello World!\n"); 
+    // Prints "Hello World!" to the screen. copied from stackoverflow.com
+    return 0;
+}
