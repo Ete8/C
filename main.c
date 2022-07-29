39448052e3375e791d420508c1f672e39cadf0aa
Srajan Rai <srajaneh@gmail.com>
2022-07-29 13:26:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,5 +4,13 @@ int main(void) {
     system("cls");
     printf("Hello World!\n"); 
     // Prints "Hello World!" to the screen. copied from stackoverflow.com
+
+
+    // printf("%d", isPalindrome(12321));
+
+    int arr[][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+
+
     return 0;
 }
