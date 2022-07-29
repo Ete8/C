68665a1dc3b1443abe339b2c3afe436c5b6afbe1
Srajan Rai <srajaneh@gmail.com>
2022-07-29 13:26:12 +0530

    Update base.h

--- base.h
+++ base.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define PIE 3.14159265358979323846
 
@@ -26,16 +27,31 @@ int sum(int n) {
     return n * (n + 1) / 2; 
 } // sum of digits copilot
 
-// int sum_of_digits(int n) {
-//     if (n < 10) {
-//         return n;
-//     }
-//     return n % 10 + sum_of_digits(n / 10);
-// } // sum of digits copilot
+int sum_of_digits(int n) {
+    if (n < 10) {
+        return n;
+    }
+    return n % 10 + sum_of_digits(n / 10);
+} // sum of digits copilot
 
-// int sum_of_digits_recursive(int n) {
-//     if (n < 10) {
-//         return n;
-//     }
-//     return n % 10 + sum_of_digits_recursive(n / 10);
-// } // sum of digits copilot
+int sum_of_digits_recursive(int n) {
+    if (n < 10) {
+        return n;
+    }
+    return n % 10 + sum_of_digits_recursive(n / 10);
+} // sum of digits copilot
+
+bool isPalindrome(int x) {
+    if(x<0) return false; 
+    else {
+        int rev=0, c=x;
+        for(int i=0; i< itoa(); i++) {
+            rev = rev*pow(10, i)+c%10;
+            if(c<10) c=0;
+            else c/=10;
+        }
+       printf("%d %d\n", x, rev);
+        return x==rev;
+    }
+} // isPalindrome copilot
+
