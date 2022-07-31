00c9dc276a644b7d4af79a9da0fcea27c9b42a27
Srajan Rai <srajaneh@gmail.com>
2022-07-31 07:25:54 +0530

    Create 2-Printf.c

---
+++ Programs/1 Basic/2-Printf.c
@@ -0,0 +1,11 @@
+#include "basic.h"
+
+int main(void) {
+    clear_screen();
+    printf("Bunch of Printf Examples\n");
+    printf("2nd Printf Example\n");
+    printf("3rd\n");
+    printf("and so on......\n");
+    printf("okay\n");
+    return 0;
+}
