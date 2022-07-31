091f502ba0aa6fc81cb26f80477a1bdc79915e42
Srajan Rai <srajaneh@gmail.com>
2022-07-31 07:26:19 +0530

    Create 13-comma.c

---
+++ Programs/1 Basic/13-comma.c
@@ -0,0 +1,11 @@
+#include "basic.h" 
+
+int main(void) {
+    clear_screen();
+    int a, b, c;
+    a = (2, 4, 50);
+    b = (2, 4);
+    c = (2);
+    printf("%d %d %d\n", a, b, c);
+    return 0;
+}
